87206bc2f79d22e588492611c6a28049261e6581
Carel Jonkhout <carel.jonkhout@gmail.com>
2019-08-05 13:05:30 +0200

    fixed testbruss_HP1

--- Continuer/rearr.m
+++ Continuer/rearr.m
@@ -5,6 +5,6 @@ function [x,p,k] = rearr(x0)
 % Rearranges x0 into coordinates (x) and parameters (p)
 global cds
 p = cds.P0;
-p(cds.ActiveParams) = x0(end-cds.nap+1:end);
+p(cds.ActiveParams) = x0(cds.ncoo+(1:cds.nap));
 x = x0(1:cds.ncoo);
 k = x0(end);
